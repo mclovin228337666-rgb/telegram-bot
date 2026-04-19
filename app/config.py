e04beb105fe619c89583abb23979aefabb4f2497
mclovin228337666-rgb <mclovin228337666@gmail.com>
2026-04-19 18:06:27 +0300

    Update config.py

--- app/config.py
+++ app/config.py
@@ -3,5 +3,5 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BOT_TOKEN = os.getenv("8547678247:AAEX6WtY5Ip7pIQZoNuNi_LQOEO5LpOhBSU")
-WEBHOOK_URL = os.getenv("https://telegram-bot-xiwi-mkhy.onrender.com")
+os.getenv("BOT_TOKEN")
+os.getenv("WEBHOOK_URL")
